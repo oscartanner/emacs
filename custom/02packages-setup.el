beaab6ca04978cc49487af3ebe596ceb471fcf9e
Oscar José Fernandes Tanner <oscar.tanner@payconiq.com>
2019-03-27 16:43:12 +0100

    Remove projectile custom keybings

--- custom/02packages-setup.el
+++ custom/02packages-setup.el
@@ -29,9 +29,9 @@
 (setq projectile-enable-caching t)
 (setq projectile-completion-system 'grizzl)
 ;; Press Command-p for fuzzy find in project
-(global-set-key (kbd "s-p") 'projectile-find-file)
+;; (global-set-key (kbd "s-p") 'projectile-find-file)
 ;; Press Command-b for fuzzy switch buffer
-(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
+;; (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
 ;; Git project folders
 (setq projectile-project-search-path '("~/dev/"))
 
