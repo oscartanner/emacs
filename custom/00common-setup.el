82a2c46bfa33938515a359b6e3827e76e0a1b161
Oscar José Fernandes Tanner <oscar.tanner@payconiq.com>
2019-03-27 16:42:07 +0100

    Add method to comment or uncomment line or region

--- custom/00common-setup.el
+++ custom/00common-setup.el
@@ -70,6 +70,18 @@
       (setq mac-option-modifier nil)))
   )
 
+;; Comment and uncomment line or region of the code
+(defun comment-or-uncomment-line-or-region ()
+  "Comments or uncomments the current line or region."
+  (interactive)
+  (if (region-active-p)
+      (comment-or-uncomment-region (region-beginning) (region-end))
+    (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    )
+  )
+(global-set-key (kbd "s-/") 'comment-or-uncomment-line-or-region)
+
+
 ;; Highlight matching parentheses
 (show-paren-mode 1)
 
