6f22be818fa768cbc1a792a0c091087e92e2afa5
Oscar José Fernandes Tanner <oscar.tanner@payconiq.com>
2019-03-27 16:53:05 +0100

    Remove Marmelade package archive

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
 
 ;; Repositories
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 
