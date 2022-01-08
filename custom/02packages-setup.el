8735027b33791f600ab9265efb88133325d48354
Oscar Tanner <oscar.tanner@payconiq.com>
2022-01-08 16:35:56 +0100

    Fix projectile config

--- custom/02packages-setup.el
+++ custom/02packages-setup.el
@@ -24,15 +24,9 @@
   (sp-local-pair "<" ">"))
 
 ;; Projectile Mode
-(require 'grizzl)
-(projectile-global-mode)
+;; (require 'grizzl)
+(projectile-global-mode +1)
 (setq projectile-enable-caching t)
-(setq projectile-completion-system 'grizzl)
-;; Press Command-p for fuzzy find in project
-;; (global-set-key (kbd "s-p") 'projectile-find-file)
-;; Press Command-b for fuzzy switch buffer
-;; (global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
-;; Git project folders
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (setq projectile-project-search-path '("~/dev/"))
 
